6dc1b47d1fd4a2b9cddb69f5594aa68088fd48eb
Bartosz Staniek <bartosz@staniek.net>
2025-03-20 18:47:57 +0100

    (db) add /db/users route to test if prisma works

--- apps/hono/src/main.ts
+++ apps/hono/src/main.ts
@@ -1,9 +1,33 @@
 import { serve } from '@hono/node-server';
 import { Hono } from 'hono';
+import { PrismaClient } from '@prisma/client';
 
-const app = new Hono();
+const prisma = new PrismaClient();
+
+type Variables = {
+  users: {
+    id: string
+    email: string
+    password: string
+    isActive: boolean
+    createdAt: Date
+  }[]
+}
+
+const app = new Hono<{
+  Variables: Variables
+}>();
 
 app.get('/', (c) => c.text('Hello from Hono!'));
+
+app.use('/db/*', async (c, next) => {
+  const users = await prisma.user.findMany();
+  c.set('users', users);
+  await next();
+});
+
+app.get('/db/users', c =>
+  c.text(c.get('users').map(user => `id=${user.id} email=${user.email}`).join('\n')));
 
 serve({ fetch: app.fetch, port: 3000 }).on('listening', () =>
   console.log('>>> API running on http://localhost:3000/')
