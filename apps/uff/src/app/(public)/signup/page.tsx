cc724501df814c130a3f8e940e9431d0800acd34
Filip Koźlicki <filip.kozlickii@gmail.com>
2025-03-24 20:42:44 +0100

    Created SignIn & SignUp pages

---
+++ apps/uff/src/app/(public)/signup/page.tsx
@@ -0,0 +1,13 @@
+import SignupFooter from '@/components/signup-footer';
+import SignupForm from '@/components/signup-form';
+import SignupHeader from '@/components/signup-header';
+
+export default function SignUpPage() {
+  return (
+    <>
+      <SignupHeader />
+      <SignupForm />
+      <SignupFooter />
+    </>
+  );
+}
