6358533f3ad522a3975858632af2f71605e0aef8
Maksym Stepanchyk <61432929+Land-Maze@users.noreply.github.com>
2025-04-16 02:12:57 +0200

    (api) Added Hono RPC

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,16 +1,19 @@
 import { serve } from '@hono/node-server';
 import { Hono } from 'hono';
-import { auth } from './routes/auth';
-import { loggedUserMiddleware } from './middleware/user';
+import { mainRouter } from '@/uff-api-routers';
 
-const app = new Hono();
-
-app.use(loggedUserMiddleware);
-
-app.get('/', (c) => c.text('Hello from Hono!'));
-
-app.route('/auth', auth);
+const app = new Hono().route('/', mainRouter);
 
 serve({ fetch: app.fetch, port: 3000 }).on('listening', () =>
   console.log('>>> API running on http://localhost:3000/')
+  // client.api.v1.user.me
+  //   .$get()
+  //   .then((res) => {
+  //     res.json().then((data) => {
+  //       console.log('>>> API client test', data);
+  //     });
+  //   })
+  //   .catch((err) => {
+  //     console.error('>>> API client test failed', err);
+  //   });
 );
