881e737d75e36dd52222b522471216f7bc3c6e00
Maksym Stepanchyk <61432929+Land-Maze@users.noreply.github.com>
2025-04-27 14:42:34 +0200

    Deleted comments, minor fix

--- libs/api/client/src/lib/client.ts
+++ libs/api/client/src/lib/client.ts
@@ -6,23 +6,7 @@ import { hc } from 'hono/client';
 
 // FIXME: Fix env type, and insert env UFF_API_URL
 export const client = hc<AppType>("http://uff-api:3000", {
-  fetch: async (req: RequestInfo | URL, init?: RequestInit) => {
-    // NOTE: This is for session cookie
-    // let session: undefined = undefined;
-
-    return fetch(req, {
-      ...init,
-      headers: {
-        ...init?.headers,
-        // 'cookie': session,
-      },
-    });
-
-    // NOTE: This is for session cookie
-    // try {
-    //
-    // } catch {
-    //
-    // }
-  },
+  init: {
+    credentials: 'include',
+  }
 });
