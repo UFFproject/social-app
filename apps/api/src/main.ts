430346c301dfdee93ea6c26e2b8798a50e6a21c1
Maksym Stepanchyk <61432929+Land-Maze@users.noreply.github.com>
2025-04-27 14:42:34 +0200

    Deleted comments, minor fix

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -6,14 +6,4 @@ const app = new Hono().route('/', mainRouter);
 
 serve({ fetch: app.fetch, port: 3000 }).on('listening', () =>
   console.log('>>> API running on http://localhost:3000/')
-  // client.api.v1.user.me
-  //   .$get()
-  //   .then((res) => {
-  //     res.json().then((data) => {
-  //       console.log('>>> API client test', data);
-  //     });
-  //   })
-  //   .catch((err) => {
-  //     console.error('>>> API client test failed', err);
-  //   });
 );
