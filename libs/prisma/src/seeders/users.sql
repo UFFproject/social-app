661a805f5035a4ad9dd38fba225c8eca1faa4223
H3WII <macpod@onet.pl>
2025-05-08 19:18:14 +0200

    Seeders for users

---
+++ libs/prisma/src/seeders/users.sql
@@ -0,0 +1,10 @@
+INSERT INTO "users" ("id", "email", "password", "address", "isActive") VALUES
+                                                                         ('0196b0da-8eac-7782-989b-2cd7a3ba84c0',	'maciej@podczasi.pl',	'$2b$12$cVDBwEAPjYnMfYFH8uLrZu0W7rXyG7BxcOsy.iO0X7.RBhFpsfCmy',	NULL,	'0'),
+                                                                         ('0196b0de-37ae-74f0-80b7-6cb674af4fff',	'patrycja@tarla.pl',	'$2b$12$GEzc9TbZOmAse9iPjqY0POgUgZhnbwWVY3Tyi.tnX2HdR2Ddl2J6S',	NULL,	'0'),
+                                                                         ('0196b0df-211f-7372-8052-6c56c330b12f',	'eryk@robak.pl',	'$2b$12$J5XHYB2v6Y7Bdkb5f27F9euU2IYKfBpAFdDqdmpgTwtjlwHGefqu2',	NULL,	'0'),
+                                                                         ('0196b0e0-240d-7360-a2b7-3455fbd92e0f',	'bartosz@walkiewicz.pl',	'$2b$12$gaqKrRBmlilTyaBWF21RSuLe8CBNhHHXnxaKybXiWnsamMX7rXR4e',	NULL,	'0'),
+                                                                         ('0196b0e1-5724-7b62-b7be-b7917011aaee',	'tamara@laskiewicz.pl',	'$2b$12$caIl0VCfC21fn0fcik7/FuxDPFzEl1HGw69eFGlig9aAD/JW4yd96',	NULL,	'0'),
+                                                                         ('0196b0e2-12ea-7a61-967a-a269e3d5ab04',	'dominik@cebula.pl',	'$2b$12$RKjxIQHk/T.QbIepQWlPXucEFix6u12oL9JNn3eg7dP0xDKWM66Sq',	NULL,	'0'),
+                                                                         ('0196b0e4-9514-78f0-a48c-fcc3189ec5db',	'olga@kubek.pl',	'$2b$12$C/9F8fQiKVzZY9f9ZOp/e.8Bcp5Ut/5J2iNxbCNFG1mB9ZTHPrk92',	NULL,	'0'),
+                                                                         ('0196b0e5-8950-7160-acbd-8a885b9afb7c',	'sebastian@klocek.pl',	'$2b$12$EPv67fVEAPUS.y5Kcs6WKO4tY.Gu17S/NRmMbqW0QawdUmbFKm.eu',	NULL,	'0');
+-- 2025-05-08 17:15:59 UTC
